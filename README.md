87c641bd6f01f32745deaa4a48961c36426429de
Gabriela <gmedvets@ucsc.edu>
2019-07-03 04:38:37 -0700

    initialized the Bootstrap template for our home page, added the template for /games

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # English-with-Sammy
+This repository is created for our CMPS 115 project at UC Santa Cruz.
+We are implementing a website where users could learn English words by playing short interactive games.
+This project is expected to be completed in 5 weeks according to our SCRUM plan.
+
+### For local development and testing
+1. Clone the repository
+2. Install [npm, node](https://nodejs.org/en/download/), and [firebase](https://firebase.google.com/docs/cli#install_the_firebase_cli) (skip if you have that)
+3. Go to "/functions" and run `npm update`
+4. Within the same folder run `npm run serve`
+5. Your local Sammy will be hosted on a `1150` port
+6. Visit `localhost:1150` in your browser and have fun!
+
+### Links to project-related resources elsewhere on the web
+[Trello](https://trello.com/invite/b/h2H8jaYs/1989a603fe8ccbbc805ff282e2aed24f/overview-of-the-project)
+
+### Contributors
+Gabriela Medvetska (Product Owner)
+Fawaz Chaudhry (Scrum Master)
+Juan Lee
+Kyungmo Kim
+Jonathan Huey
---
+++ functions/index.js
@@ -0,0 +1,37 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+// const PORT = process.env.PORT || 1750
+
+const app = express();
+
+// use absolute path /css/custom.css
+// (with a trailing slash for loading static files on the client side
+app.use(express.static(path.join(__dirname, 'public/')));
+app.set('/views', path.join(__dirname, './views/'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    res.render('pages/index.ejs');
+});
+
+// The / and /home routes need to be edited in Sprint 3 so that users who are logged in
+// are redirected to the dashboard.ejs and those that are not to index.js
+app.get('/', (req, res) => res.render('pages/index.ejs'));
+app.get('/home', (req, res) => res.render('pages/index.ejs'));
+app.get('/dashboard', (req, res) => res.render('pages/dashboard.ejs'));
+
+
+// Sprint 1 functionality
+app.get('/games', (req, res) => res.render('pages/games.ejs'));
+app.get('/matchingPairs', (req, res) => res.render('pages/games/mPairs.ejs'));
+
+// Sprint 2 functionality
+app.get('/crosswords', (req, res) => res.render('pages/games/crosswords.ejs'));
+
+// Sprint 3 functionality
+app.get('/login', (req, res) => res.render('pages/login.ejs'));
+app.get('/signup', (req, res) => res.render('pages/signup.ejs'));
+
+exports.app = functions.https.onRequest(app);
